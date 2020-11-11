e5d7efec03198283e00ab8c73406529cf90e9b22
sunuwara <sunuwara@mail.gvsu.edu>
2020-11-11 16:34:13 -0500

    fixed sample program 1

--- Lab08/sample_program1.c
+++ Lab08/sample_program1.c
@@ -1,3 +1,5 @@
+/* REVISED SAMPLE PROGRAM 1 WITH SEGMENTATION FAULT FIXED */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,9 +10,12 @@ int main()
 {
     char *data1;
 
-    data1 = malloc (SIZE);
-    printf ("Please input username: ");
-    scanf ("%s", data1);
+    data1 = malloc(SIZE);
+    printf ("Please input username (will only take %d characters): ", (SIZE - 1));
+    
+    // Only 15 characters of input will be read
+    scanf ("%15s", data1);
+    
     printf ("you entered: %s\n", data1);
     free (data1);
     return 0;
