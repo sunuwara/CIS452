77fb22d64d62f0793e7e5444d374f698e501fabc
sunuwara <sunuwara@mail.gvsu.edu>
2020-11-30 22:32:34 -0500

    modify to add check for directory or not

--- Lab10/sample_program1.c
+++ Lab10/sample_program1.c
@@ -6,18 +6,27 @@
 
 int main(int argc, char *argv[])
 {
-   struct stat statBuf;
+	struct stat statBuf;
 
-   if (argc < 2) {
-      printf ("Usage: filename required\n");
-      exit(1);
-   }
+	if (argc < 2) {
+		printf ("Usage: filename required\n");
+		exit(1);
+	}
 
-   if (stat (argv[1], &statBuf) < 0) {
-      perror ("huh?  there is ");
-      exit(1);
-   }
-
-   printf ("value is: %u\n", statBuf.st_mode);
-   return 0;
+	if (stat (argv[1], &statBuf) < 0) {
+		perror ("huh?  there is ");
+		exit(1);
+	}
+	
+	printf ("value is: %u\n", statBuf.st_mode);
+	
+	if (S_ISDIR(statBuf.st_mode)) {
+		/* Handle directory */
+		printf("Input is a directory!\n");
+	}
+	else {
+		printf("Input is not a directory!\n");
+	}
+	
+	return 0;
 }
