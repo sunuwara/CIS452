dc213250a4c945aa40a2b86f0e0fb13f1b9528f5
sunuwara <sunuwara@mail.gvsu.edu>
2020-11-30 23:39:16 -0500

    add main program

--- Lab10/sample_program2.c
+++ Lab10/sample_program2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -8,11 +9,18 @@ int main()
 {
 	DIR *dirPtr;
 	struct dirent *entryPtr;
-
+	struct stat statBuf;
+	
 	dirPtr = opendir (".");
-
-	while ((entryPtr = readdir (dirPtr)))
-		printf ("%-20s\n", entryPtr->d_name);
+	
+	while ((entryPtr = readdir (dirPtr))) {
+		if (stat (entryPtr->d_name, &statBuf) < 0) {
+			perror ("huh?  there is ");
+			exit(1);
+		}
+		
+		printf ("%ld bytes	%-20s\n", statBuf.st_size, entryPtr->d_name);
+	}
 	
 	closedir (dirPtr);
 	
