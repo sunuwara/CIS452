a4871d9f0c2616f57bb15910692ae22373bc6eb8
sunuwara <sunuwara@mail.gvsu.edu>
2020-12-01 05:09:01 -0500

    save current directory work

--- Lab10/simple_ls.c
+++ Lab10/simple_ls.c
@@ -9,73 +9,46 @@
 
 void emptyArgvOne(DIR *dirPtr);
 void nOption(DIR *dirPtr);
+void iOption(DIR *dirPtr);
 char *createPermissionsStr(struct stat statBuf);
 
 int main(int argc, char *argv[])
 {
     DIR *dirPtr;
-    // struct dirent *entryPtr;
-    // struct stat statBuf;
+    //struct dirent *ep; // entry pointer
 
-    // arguments need to be <= 3
     if (argc > 3)
     {
         printf("Usage: too many arguments\n");
         exit(1);
     }
 
-    // list current directory contents
+    // list current directory contents or argv[2] directory contents
     if (argc <= 2)
-    {
         dirPtr = opendir(".");
-
-        // if argv[1] is " " then just print out name of each file
-        if (argc == 1)
-            emptyArgvOne(dirPtr);
-
-        // if argv[1] is -n then give user and group ID for each file (with other info)
-        else if (!strcmp(argv[1], "-n"))
-            nOption(dirPtr);
-
-        // // if argv[1] is -i then give inode # for each file
-        // else if (!strcmp(argv[1], "-i"))
-        // {
-        // }
-
-        // invalid argument
-        else
-        {
-            printf("Usage: invalid argument\n");
-            exit(1);
-        }
+    else
+    {
+        dirPtr = opendir(argv[2]);
+        printf("one: %s\n", argv[2]);
+        
+        
     }
 
-    // // list argv[2] directory contents
-    // else
-    // {
-    //     dirPtr = opendir(argv[2]);
-    //     // if argv[1] is " " then just print out name of each file
-    //     if (argc == 1)
-    //     {
-    //     }
-
-    //     // if argv[1] is -n then give user and group ID for each file (with other info)
-    //     else if (strcmp(argv[1], "-n") == 0)
-    //     {
-    //     }
-
-    //     // if argv[1] is -i then give inode # for each file
-    //     else if (strcmp(argv[1], "-i") == 0)
-    //     {
-    //     }
-
-    //     // invalid argument
-    //     else
-    //     {
-    //         printf("Usage: invalid argument\n");
-    //         exit(1);
-    //     }
-    // }
+    // if there is no argv[1] then just print out name of each file
+    // if argv[1] is "-n" then give user and group ID for each file (with other info)
+    // if argv[1] is "-i" then give inode # for each file
+    if (argc == 1)
+        emptyArgvOne(dirPtr);
+    else if (!strcmp(argv[1], "-n"))
+        nOption(dirPtr);
+    else if (!strcmp(argv[1], "-i"))
+        iOption(dirPtr);
+    else
+    {
+        // invalid argument
+        printf("Usage: invalid argument\n");
+        exit(1);
+    }
 
     closedir(dirPtr);
 
@@ -112,31 +85,54 @@ void nOption(DIR *dirPtr)
         if (!strcmp(ep->d_name, ".") || !strcmp(ep->d_name, ".."))
             continue;
 
-	printf(createPermissionsStr(sb));
-	
         strftime(timeBuffer, 80, "%b %d %H:%M", localtime(&sb.st_ctim.tv_sec));
-        printf("%o %ld %u %u %5ld %5s %5s\n", sb.st_mode, sb.st_nlink, sb.st_uid, sb.st_gid, sb.st_size, timeBuffer, ep->d_name);
+
+        printf("%s ", createPermissionsStr(sb));
+        printf("%lu ", sb.st_nlink);
+        printf("%u ", sb.st_uid);
+        printf("%u ", sb.st_gid);
+        printf("%5ld ", sb.st_size);
+        printf("%5s ", timeBuffer);
+        printf("%5s\n", ep->d_name);
     }
 }
 
-char *createPermissionsStr(struct stat statBuf) 
+void iOption(DIR *dirPtr)
 {
-	char str[10];
-	char temp;
-	
-	strcat(str, (S_ISDIR(statBuf.st_mode)) ? "d" : "-");
-	strcat(str, (statBuf.st_mode & S_IRUSR) ? "r" : "-");
-	strcat(str, (statBuf.st_mode & S_IWUSR) ? "w" : "-");
-	strcat(str, (statBuf.st_mode & S_IXUSR) ? "x" : "-");
-	strcat(str, (statBuf.st_mode & S_IRGRP) ? "r" : "-");
-	strcat(str, (statBuf.st_mode & S_IWGRP) ? "w" : "-");
-	strcat(str, (statBuf.st_mode & S_IXGRP) ? "x" : "-");
-	strcat(str, (statBuf.st_mode & S_IROTH) ? "r" : "-");
-	strcat(str, (statBuf.st_mode & S_IWOTH) ? "w" : "-");
-	strcat(str, (statBuf.st_mode & S_IXOTH) ? "x" : "-");
-	
-	return str;
+    struct dirent *ep; // entry pointer
+    struct stat sb;    // stat buffer
+
+    while ((ep = readdir(dirPtr)))
+    {
+        if (stat(ep->d_name, &sb) < 0)
+        {
+            perror("unsuccessful stat");
+            exit(1);
+        }
+
+        if (!strcmp(ep->d_name, ".") || !strcmp(ep->d_name, ".."))
+            continue;
+
+        printf("%ld %8s\n", sb.st_ino, ep->d_name);
+    }
 }
 
+char *createPermissionsStr(struct stat statBuf)
+{
+    static char str[11];
 
+    str[0] = (S_ISDIR(statBuf.st_mode)) ? 'd' : '-';
+    str[1] = (statBuf.st_mode & S_IRUSR) ? 'r' : '-';
+    str[2] = (statBuf.st_mode & S_IWUSR) ? 'w' : '-';
+    str[3] = (statBuf.st_mode & S_IXUSR) ? 'x' : '-';
+    str[4] = (statBuf.st_mode & S_IRGRP) ? 'r' : '-';
+    str[5] = (statBuf.st_mode & S_IWGRP) ? 'w' : '-';
+    str[6] = (statBuf.st_mode & S_IXGRP) ? 'x' : '-';
+    str[7] = (statBuf.st_mode & S_IROTH) ? 'r' : '-';
+    str[8] = (statBuf.st_mode & S_IWOTH) ? 'w' : '-';
+    str[9] = (statBuf.st_mode & S_IXOTH) ? 'x' : '-';
+    str[10] = '\0';
+
+    return str;
+}
 
